7e3a23befb8be5e8f452d6ea289b151fe6b90cf8
IvanKMK <kumeyko@hotmail.com>
2020-08-19 22:28:07 +0100

    Added filter to the admin panel

--- property/admin.py
+++ property/admin.py
@@ -6,8 +6,9 @@ from .models import Flat
 class FlatAdmin(admin.ModelAdmin):
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
-    search_fields = ['town','address', 'owner']
+    search_fields = ['town','address', 'owner', 'new_building']
     readonly_fields = ['created_at']
+    list_filter = ('new_building', 'rooms_number', 'has_balcony')
  
 
 admin.site.register(Flat, FlatAdmin)
